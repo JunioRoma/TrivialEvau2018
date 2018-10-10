b96aaddeb15487ae7256d0c3e267516e7ab94e4d
Gonzalo Sáez Martí <Gonzalo Sez Mart@192.168.145.130>
2018-10-10 16:41:52 +0200

    arreglado juego.php

--- juego.php
+++ juego.php
@@ -1,96 +1,59 @@
-
 <?php
 session_start();
-
+//capturo los valores de los parámetros que me han ido pasados
+//desde aplicacion.php
 include ('misFunciones.php');
-//Capturo los valores de los parametros q se me han sido pasados desde app.php
-$vidas = ($_POST['vidas']);
-$correctas = ($_POST['correctas']);
-$tema = ($_POST['tema']);
-
+$vidas = $_POST['vidas'];
+$correctas = $_POST['correctas'];
+$tema = $_POST['tema'];
 $mysqli = conectaBBDD();
-$resultadoQuery = $mysqli->query("SELECT * FROM preguntas WHERE tema = '$tema' ");
+$resultadoQuery = $mysqli->query("SELECT * FROM preguntas WHERE tema = '$tema'");
 $numPreguntas = $resultadoQuery->num_rows;
-
-//declaro un array en php para gurdar el resultado de la query
+//declaro un array en php para guardar el resultado de la query
 $listaPreguntas = array();
-
-//cargo todas las filas del resultado de la query en el array
+//Cargo todas las filas de la query en el array
 for ($i = 0; $i < $numPreguntas; $i++) {
-//Leo una fila del resultado de la query
-    $registro = $resultadoQuery->fetch_array();
-    $listaPreguntas[$i][0] = $registro['numero'];
-    $listaPreguntas[$i][1] = $registro['enunciado'];
-    $listaPreguntas[$i][2] = $registro['r1'];
-    $listaPreguntas[$i][3] = $registro['r2'];
-    $listaPreguntas[$i][4] = $registro['r3'];
-    $listaPreguntas[$i][5] = $registro['r4'];
-    $listaPreguntas[$i][6] = $registro['correcta'];
+    $r = $resultadoQuery->fetch_array(); //leo una fila del resultado de la query
+    $listaPreguntas[$i][0] = $r['numero'];
+    $listaPreguntas[$i][1] = $r['enunciado'];
+    $listaPreguntas[$i][2] = $r['r1'];
+    $listaPreguntas[$i][3] = $r['r2'];
+    $listaPreguntas[$i][4] = $r['r3'];
+    $listaPreguntas[$i][5] = $r['r4'];
+    $listaPreguntas[$i][6] = $r['correcta'];
 }
 $preguntaActual = rand(0, $numPreguntas - 1);
 ?>
 
 <div>
-    <p><a  class="btn btn-block btn-dark disabled" >Demuestra que estás listo para la EVAU</a></p>
-    <p><a id="sigue1" class="btn btn-block disabled"> <?php echo $_SESSION['nombreUsuario']?> </a></p>
-    <p><a  class="btn btn-block btn-warning" onclick="volver();">Volver al Menú</a></p>
-    <p><a id="sigue1" class="btn btn-block btn-primary"> <?php echo $tema; ?> </a></p>
+    <p><a class="btn btn-block btn-dark disabled">DEMUESTRA QUE ESTAS LISTO PARA LA EVAU</a></p>
+    <p><a class="btn btn-block btn-dark disabled"><?php echo $_SESSION['nombreUsuario'] ?></a></p>
+    <p><a class="btn btn-block btn-warning" onclick="volver();">Volver al Menú</a></p>
 
+    <p><a id="sigue1" class="btn btn-block btn-primary"><?php echo $tema; ?></a></p>
 
-    <p><a  id="encunciado" class="btn btn-block btn-info" ></a></p>
+    <p><a id="enunciado" class="btn btn-block btn-primary"></a></p>
 
-    <p><a id="r1" class="btn btn-block btn-success" ></a></p>
-    <p><a id="r2" class="btn btn-block btn-success" ></a></p>
-    <p><a id="r3" class="btn btn-block btn-success" ></a></p>
-    <p><a id="r4" class="btn btn-block btn-success" ></a></p>
-
-
-
+    <p><a id="r1" class="btn btn-block btn-success"></a></p>
+    <p><a id="r2" class="btn btn-block btn-success"></a></p>
+    <p><a id="r3" class="btn btn-block btn-success"></a></p>
+    <p><a id="r4" class="btn btn-block btn-success"></a></p>
 </div>
 
-
 <script>
-
     function volver() {
-
-        $('#principal').load('app.php');
-
-
+        $('#principal').load('aplicacion.php');
     }
-
+    //Cargo el array php de preguntas en una variable javascript
     var listaPreguntas = <?php echo json_encode($listaPreguntas); ?>;
-
     var numeroPregunta = Math.floor(Math.random() * listaPreguntas.length);
     sigue();
-
-
-
     function sigue() {
-       
         numeroPregunta = Math.floor(Math.random() * listaPreguntas.length);
-        $('#enunciado').text(listaPreguntas[1]);
-        $('#r1').text(listaPreguntas[2]).click(function () {
-            sigue();
-        });
-        $('#r2').text(listaPreguntas[3]).click(function () {
-            sigue();
-        });
-        $('#r3').text(listaPreguntas[4]).click(function () {
-            sigue();
-        });
-        $('#r4').text(listaPreguntas[5]).click(function () {
-            sigue();
-        });
-
+        $('#enunciado').text(listaPreguntas[numeroPregunta][1]);
+        $('#r1').text(listaPreguntas[numeroPregunta][2]).click(function () {sigue();});
+        $('#r2').text(listaPreguntas[numeroPregunta][3]).click(function () {sigue();});
+        $('#r3').text(listaPreguntas[numeroPregunta][4]).click(function () {sigue();});
+        $('#r4').text(listaPreguntas[numeroPregunta][5]).click(function () {sigue();});
     }
-
-
 </script>
-
-
-/* 
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
-
