e9066f4827996fe4f7c96dac7d463a24714d3791
Administrator <Administrator@192.168.186.140>
2018-10-17 16:35:17 +0200

    Añadido el registro con una granfuncionalidad sin encriptar y solucionado los fallos

---
+++ registro-usuario.php
@@ -0,0 +1,82 @@
+
+
+
+<?php
+
+$host_db = "localhost";
+
+$user_db = "root";
+
+$pass_db = "";
+
+$db_name = "evau2018";
+
+$tbl_name = "Usuarios";
+
+
+
+$form_pass = $_POST['userPass'];
+
+
+$hash = password_hash($form_pass, userPass);
+
+
+
+$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+
+
+if ($conexion->connect_error) {
+
+    die("La conexion falló: " . $conexion->connect_error);
+}
+
+
+
+$buscarUsuario = "SELECT * FROM $tbl_name WHERE nombreUsuario = '$_POST[nombreUsuario]' ";
+$buscarContra = "SELECT * FROM $tbl_name WHERE userPass = '$_POST[userPass]' ";
+
+
+
+
+$result = $conexion->query($buscarUsuario);
+
+
+
+$count = mysqli_num_rows($result);
+
+
+
+if ($count == 1) {
+
+    echo "<br />" . "El Nombre de Usuario ya a sido tomado." . "<br />";
+
+
+
+    echo "<a href='index.php'>Por favor escoga otro Nombre</a>";
+} else {
+
+
+    $query = "INSERT INTO Usuarios (nombreUsuario, userPass)
+
+           VALUES ('$_POST[nombreUsuario]', '$_POST[userPass]')";
+
+
+
+    if ($conexion->query($query) === TRUE) {
+
+
+
+        echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
+
+        echo "<h4>" . "Bienvenido: " . $_POST['nombreUsuario'] . "</h4>" . "\n\n";
+
+        echo "<h5>" . "Hacer Login: " . "<a href='login.'>app.php</a>" . "</h5>";
+    } else {
+
+        echo "Error al crear el usuario." . $query . "<br>" . $conexion->error;
+    }
+}
+
+mysqli_close($conexion);
+?>
--- registro.php
+++ registro.php
@@ -1,73 +1,66 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
+
+
+
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-        <title>PRUEBA DE PHP CON BOOTSTRAP</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/CSS.css" rel="stylesheet" type="text/css"/>
+
+        <title>Registrar Usuario</title>
+
+        <meta charset = "utf-8">
 
     </head>
-    <body style="background-color:  #9932CC; color:white; ">
 
-        <div class="container" id="principal">
-            <div class ="row">
-                <div id="banner" class="col-12">
-                    <p><a class="btn btn-block btn-info disabled" style="color: white" >Bienvenido al Trivial De La Evau 2018</a></p>
-                </div>
-                <div id="botonLogin" class="col-9">
-                    <button onclick="showModalDialog()" style="background-color:#9932CC" ><img src="homer.png"></button>
-                    <h1 id="enunciadoRegistro">HAZ CLICK PARA REGISTRARTE</h1>
-                </div>
-            </div>
-            <div class ="row">
-                <div class="col-4">
-                </div>
-                <div id="login" class="col-4">
-                    <br/><br/>
-                    <input id ="cajaNombre" class="form-control" type="text" placeholder="Usuario" required="required">
-                    <br/>
-                    <input id ="cajaPassword" class="form-control" type="password" placeholder="Contraseña">
-                    <br/>
-                    <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
-                    <br/><br/>
-                </div>
-                <div class="col-4">
-                </div>
-            </div>
-        </div>
-    </body>
-    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
 
-    <script>
-                        // document ready se ejecuta cuando toda la página se ha cargado correctamente
-                        $(document).ready(function () {
-                            //$('#cajaNombre').hide();
 
-//                            $('#login').hide();
+    <body>
 
-                        });
 
-                        $.validate({
-                            lang: 'es'
-                        });
 
-                        //Mostrar login pulsando a homer
-//                        if ($('#botonLogin').click(function)) {
-//                            $('#login').show();
-//                        }
+        <header>
+            <h1>Registrate en nuestro fabuloso website:</h1>
 
-                        $('#boton1').click(function () {
-                            //leo el contenido de las cajas de nombre y contraseña
-                            var _cajaNombre = $('#cajaNombre').val();
-                            var _cajaPassword = $('#cajaPassword').val();
+        </header>
 
-                            $('#principal').load("login.php", {
-                                cajaNombre: _cajaNombre,
-                                cajaPassword: _cajaPassword
-                            });
-                        });
-    </script>
+
+
+        <form action="registro-usuario.php" method="post"> 
+
+
+
+            <hr />
+
+            <h3>Crea una cuenta</h3>
+
+
+
+            <!--Nombre Usuario-->
+
+            <label for="nombre">Nombre de Usuario:</label><br>
+
+            <input type="text" name="nombreUsuario" maxlength="32" required>
+
+            <br/><br/>
+
+
+
+            <!--Password-->
+
+            <label for="pass">Password:</label><br>
+
+            <input type="password" name="userPass" maxlength="8" required>
+
+            <br/><br/>
+
+            <input type="submit" name="submit" value="Registrarme">
+            <input type="reset" name="clear" value="Borrar">
+
+        </form>
+
+
+    </footer>
+
+</body>
+
 </html>
